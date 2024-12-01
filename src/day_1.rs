e66491cb3d508c7b76b480b72c96203484db4e28
Kevin Calopisis <kevincal@umich.edu>
2024-12-01 08:04:09 -0500

    day 1 done

--- src/day_1.rs
+++ src/day_1.rs
@@ -1,7 +1,29 @@
+use std::io::BufRead;
+
 pub fn part_1() -> usize {
-    0
+    let reader = std::io::BufReader::new(std::fs::File::open("input/day_1.txt").unwrap());
+    let mut lhs: Vec<usize> = Vec::new();
+    let mut rhs: Vec<usize> = Vec::new();
+    reader.lines().map(|l| l.unwrap()).for_each(|line| {
+        let mut sw = line.split_whitespace();
+        lhs.push(sw.next().unwrap().parse::<usize>().unwrap());
+        rhs.push(sw.next().unwrap().parse::<usize>().unwrap());
+    });
+    lhs.sort();
+    rhs.sort();
+    (0..lhs.len()).map(|i| (rhs[i]).abs_diff(lhs[i])).sum()
 }
 
 pub fn part_2() -> usize {
-    0
+    let reader = std::io::BufReader::new(std::fs::File::open("input/day_1.txt").unwrap());
+    let mut lhs: Vec<usize> = Vec::new();
+    let mut rhs: Vec<usize> = Vec::new();
+    reader.lines().map(|l| l.unwrap()).for_each(|line| {
+        let mut sw = line.split_whitespace();
+        lhs.push(sw.next().unwrap().parse::<usize>().unwrap());
+        rhs.push(sw.next().unwrap().parse::<usize>().unwrap());
+    });
+    lhs.iter()
+        .map(|elem| *elem * rhs.iter().filter(|e| **e == *elem).count())
+        .sum()
 }
