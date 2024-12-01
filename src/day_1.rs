4d13dd36355eedf36c489fb19808adc908927a99
Kevin Calopisis <kevincal@umich.edu>
2024-12-01 08:13:28 -0500

    used a hashmap instead of vector ooooh

--- src/day_1.rs
+++ src/day_1.rs
@@ -17,13 +17,14 @@ pub fn part_1() -> usize {
 pub fn part_2() -> usize {
     let reader = std::io::BufReader::new(std::fs::File::open("input/day_1.txt").unwrap());
     let mut lhs: Vec<usize> = Vec::new();
-    let mut rhs: Vec<usize> = Vec::new();
+    let mut rhs: std::collections::HashMap<usize, usize> = std::collections::HashMap::new();
     reader.lines().map(|l| l.unwrap()).for_each(|line| {
         let mut sw = line.split_whitespace();
         lhs.push(sw.next().unwrap().parse::<usize>().unwrap());
-        rhs.push(sw.next().unwrap().parse::<usize>().unwrap());
+        *rhs.entry(sw.next().unwrap().parse::<usize>().unwrap())
+            .or_default() += 1;
     });
     lhs.iter()
-        .map(|elem| *elem * rhs.iter().filter(|e| **e == *elem).count())
+        .map(|elem| *elem * *rhs.entry(*elem).or_default())
         .sum()
 }
