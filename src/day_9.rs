6da3117c7d8d839d30d736872ecf524f5d954d30
kevincal1226 <kevincal@umich.edu>
2024-12-09 10:30:50 -0500

    part 1 done

--- src/day_9.rs
+++ src/day_9.rs
@@ -3,7 +3,7 @@ use std::io::{BufRead, BufReader};
 
 pub fn part_1(file: String) -> usize {
     let mut reader = BufReader::new(File::open(file).unwrap()).lines();
-    let mut ids: Vec<usize> = Vec::new();
+    let mut ids: Vec<(usize, usize)> = Vec::new();
     let bullshit: Vec<char> = reader
         .next()
         .unwrap()
@@ -12,7 +12,7 @@ pub fn part_1(file: String) -> usize {
         .enumerate()
         .flat_map(|(i, s)| {
             if i % 2 == 0 {
-                ids.push(s.to_digit(10).unwrap() as usize);
+                ids.push((i / 2, s.to_digit(10).unwrap() as usize));
                 "x".repeat(s.to_digit(10).unwrap() as usize)
                     .chars()
                     .collect::<Vec<char>>()
@@ -24,9 +24,46 @@ pub fn part_1(file: String) -> usize {
         })
         .collect();
     let mut sum = 0;
+    for i in 0..bullshit.len() {
+        if ids.is_empty() {
+            break;
+        }
+        if bullshit[i] == 'x' {
+            sum += i * ids[0].0;
+            ids[0].1 -= 1;
+            if ids[0].1 == 0 {
+                ids.remove(0);
+            }
+        } else {
+            sum += i * ids.last().unwrap().0;
+            ids.last_mut().unwrap().1 -= 1;
+            if ids.last().unwrap().1 == 0 {
+                ids.pop();
+            }
+        }
+    }
     sum
 }
 
 pub fn part_2(file: String) -> usize {
-    0
+    let mut reader = BufReader::new(File::open(file).unwrap()).lines();
+    let mut ids: Vec<(usize, usize)> = Vec::new();
+    let bullshit: Vec<(char, usize)> = reader
+        .next()
+        .unwrap()
+        .unwrap()
+        .chars()
+        .enumerate()
+        .map(|(i, s)| {
+            if i % 2 == 0 {
+                ids.push((i / 2, s.to_digit(10).unwrap() as usize));
+                ('x', s.to_digit(10).unwrap() as usize)
+            } else {
+                ('.', s.to_digit(10).unwrap() as usize)
+            }
+        })
+        .collect();
+    let mut sum = 0;
+    let mut starting_idx = 0;
+    sum
 }
