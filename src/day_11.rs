a70b880aec8ba17f6ab5a918cf2c5c7ee63c733a
kevincal1226 <kevincal@umich.edu>
2024-12-12 09:09:58 -0500

    day 11 done

--- src/day_11.rs
+++ src/day_11.rs
@@ -1,3 +1,5 @@
+use cached::proc_macro::cached;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -33,6 +35,26 @@ pub fn part_1(file: String) -> usize {
     stoned.len()
 }
 
+fn get_stoners(val: usize) -> Vec<usize> {
+    if val == 0 {
+        return vec![1];
+    } else if val.to_string().len() % 2 == 0 {
+        let pow = 10usize.pow((val.to_string().len() / 2) as u32);
+        return vec![val / pow, val % pow];
+    } else {
+        return vec![val * 2024];
+    }
+}
+
+#[cached]
+fn helper(val: usize, blink: usize) -> usize {
+    if blink == 75 {
+        return 1;
+    }
+    let vals = get_stoners(val);
+    vals.iter().map(|s| helper(*s, blink + 1)).sum()
+}
+
 pub fn part_2(file: String) -> usize {
     let reader = BufReader::new(File::open(file).unwrap());
     let mut stoned: Vec<usize> = reader
@@ -45,23 +67,7 @@ pub fn part_2(file: String) -> usize {
         })
         .collect();
 
-    (0..75).for_each(|it| {
-        println!("iteration: {}", it);
-        let mut i = 0;
-        while i < stoned.len() {
-            if stoned[i] == 0 {
-                stoned[i] = 1;
-            } else if stoned[i].to_string().len() % 2 == 0 {
-                let pow = 10usize.pow((stoned[i].to_string().len() / 2) as u32);
-                let new_v = stoned[i] % pow;
-                stoned.insert(i + 1, new_v);
-                stoned[i] /= pow;
-                i += 1;
-            } else {
-                stoned[i] *= 2024;
-            }
-            i += 1;
-        }
-    });
-    stoned.len()
+    let mut memo: HashMap<usize, usize> = HashMap::new();
+
+    stoned.iter().map(|x| helper(*x, 0)).sum()
 }
